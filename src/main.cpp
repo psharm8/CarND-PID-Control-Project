f6f093eb6ccad7d222c4da0e9d31fcc904516c7a
Puneet Sharma <puneet.s2004@gmail.com>
2018-06-16 00:09:05 -0400

    Updated params

--- src/main.cpp
+++ src/main.cpp
@@ -35,8 +35,8 @@ int main()
 
   PID pid_steer, pid_throttle;
   // TODO: Initialize the pid variable.
-  pid_steer.Init(0.23, 0.00024, 12.0);
-  pid_throttle.Init(0.01, 0.0, 3.0);
+  pid_steer.Init(0.28, 0.00023, 12.0);
+  pid_throttle.Init(0.005, 0.0, 3.0);
 
   h.onMessage([&pid_steer, &pid_throttle](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
